ddbedd238d4ec35d17d760f04d65c46d04a3a133
jeffwang0516 <jeff1jeffo@gmail.com>
2017-11-14 04:26:03 +0800

    fix

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -9,32 +9,6 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-local-notifications.LocalNotification",
-    "file": "plugins/cordova-plugin-local-notifications/www/local-notification.js",
-    "pluginId": "cordova-plugin-local-notifications",
-    "clobbers": [
-      "cordova.plugins.notification.local"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notifications.LocalNotification.Core",
-    "file": "plugins/cordova-plugin-local-notifications/www/local-notification-core.js",
-    "pluginId": "cordova-plugin-local-notifications",
-    "clobbers": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notifications.LocalNotification.Util",
-    "file": "plugins/cordova-plugin-local-notifications/www/local-notification-util.js",
-    "pluginId": "cordova-plugin-local-notifications",
-    "merges": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
     "id": "cordova-plugin-x-toast.Toast",
     "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
     "pluginId": "cordova-plugin-x-toast",
@@ -46,14 +20,49 @@ module.exports = [
     "id": "cordova-plugin-x-toast.tests",
     "file": "plugins/cordova-plugin-x-toast/test/tests.js",
     "pluginId": "cordova-plugin-x-toast"
+  },
+  {
+    "id": "cordova-plugin-badge.Badge",
+    "file": "plugins/cordova-plugin-badge/www/badge.js",
+    "pluginId": "cordova-plugin-badge",
+    "clobbers": [
+      "cordova.plugins.notification.badge"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "clobbers": [
+      "cordova.plugins.notification.local"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification.Core",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "clobbers": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification.Util",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "merges": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-device": "1.1.6",
-  "cordova-plugin-local-notifications": "0.0.0",
-  "cordova-plugin-x-toast": "2.6.0"
+  "cordova-plugin-x-toast": "2.6.0",
+  "cordova-plugin-badge": "0.8.5",
+  "cordova-plugin-local-notification": "0.9.0-beta.1"
 };
 // BOTTOM OF METADATA
 });
--- platforms/windows/www/cordova_plugins.js
+++ platforms/windows/www/cordova_plugins.js
@@ -36,35 +36,49 @@ module.exports = [
         ]
     },
     {
-        "id": "cordova-plugin-local-notifications.LocalNotification",
-        "file": "plugins/cordova-plugin-local-notifications/www/local-notification.js",
-        "pluginId": "cordova-plugin-local-notifications",
+        "id": "cordova-plugin-badge.Badge",
+        "file": "plugins/cordova-plugin-badge/www/badge.js",
+        "pluginId": "cordova-plugin-badge",
+        "clobbers": [
+            "cordova.plugins.notification.badge"
+        ]
+    },
+    {
+        "id": "cordova-plugin-badge.Badge.Proxy",
+        "file": "plugins/cordova-plugin-badge/src/windows/BadgeProxy.js",
+        "pluginId": "cordova-plugin-badge",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-local-notification.LocalNotification",
+        "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
+        "pluginId": "cordova-plugin-local-notification",
         "clobbers": [
             "cordova.plugins.notification.local"
         ]
     },
     {
-        "id": "cordova-plugin-local-notifications.LocalNotification.Core",
-        "file": "plugins/cordova-plugin-local-notifications/www/local-notification-core.js",
-        "pluginId": "cordova-plugin-local-notifications",
+        "id": "cordova-plugin-local-notification.LocalNotification.Core",
+        "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
+        "pluginId": "cordova-plugin-local-notification",
         "clobbers": [
             "cordova.plugins.notification.local.core",
             "plugin.notification.local.core"
         ]
     },
     {
-        "id": "cordova-plugin-local-notifications.LocalNotification.Util",
-        "file": "plugins/cordova-plugin-local-notifications/www/local-notification-util.js",
-        "pluginId": "cordova-plugin-local-notifications",
+        "id": "cordova-plugin-local-notification.LocalNotification.Util",
+        "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
+        "pluginId": "cordova-plugin-local-notification",
         "merges": [
             "cordova.plugins.notification.local.core",
             "plugin.notification.local.core"
         ]
     },
     {
-        "id": "cordova-plugin-local-notifications.LocalNotification.Proxy",
-        "file": "plugins/cordova-plugin-local-notifications/src/windows/LocalNotificationProxy.js",
-        "pluginId": "cordova-plugin-local-notifications",
+        "id": "cordova-plugin-local-notification.LocalNotification.Proxy",
+        "file": "plugins/cordova-plugin-local-notification/src/windows/LocalNotificationProxy.js",
+        "pluginId": "cordova-plugin-local-notification",
         "merges": [
             ""
         ]
@@ -75,7 +89,8 @@ module.exports.metadata =
 {
     "cordova-plugin-device": "1.1.6",
     "cordova-plugin-x-toast": "2.6.0",
-    "cordova-plugin-local-notifications": "0.0.0"
+    "cordova-plugin-badge": "0.8.5",
+    "cordova-plugin-local-notification": "0.9.0-beta.1"
 };
 // BOTTOM OF METADATA
 });
